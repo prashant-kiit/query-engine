bf3bb38e0b0a6ea34c23bda1febbce2442419e0c
Prashant Kumar Singh <prashantsingh090798@gmail.com>
2025-03-15 17:02:14 +0530

    feat: added parser

--- Database/Parser/Main.java
+++ Database/Parser/Main.java
@@ -1,44 +1,64 @@
 import java.io.*;
 import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "query1.sql";
-        Path pathToFile = Paths.get("Database/Query", fileName);
+        String sourceCode = "query1.sql";
+        Path pathToSourceCode = Paths.get("Database/Query", sourceCode);
+        List<String> symbols = Arrays.asList("select", "from", "where", "order_by", "limit"); 
 
-        // Reading and printing characters using Stream
-        StringBuilder content = new StringBuilder();
-        try (BufferedReader reader = Files.newBufferedReader(pathToFile)) {
-            int ch;
-            while ((ch = reader.read()) != -1) {
-                content.append((char) ch);
-            }
-            List<String> tokens = Arrays.asList(content.toString().split("[\\s,\\n]+"));
-            System.out.println("Tokens: " + tokens);
+        try {
+            // reading file
+            StringBuilder language = getLanguageFromSourceCode(pathToSourceCode);
 
-            StoreToken storeToken = new StoreToken();
-            
+            // tokenize using comma, white space and new line as delimiters
+            List<String> tokens = getTokenFromString(language);
 
+            // parse tokens based on symbols list
+            Map<String, List<String>> parseTree = getParseTreeFromTokens(tokens, symbols);
+
+            // Print parseTree
+            parseTree.forEach((key, value) -> System.out.println(key + " -> " + value));
         } catch (Exception e) {
             System.out.println("Error reading file: " + e);
         }
     }
-}
 
-enum Lexicon {
-    SELECT("select"),
-    FROM("from"),
-    WHERE("where");
+    private static Map<String, List<String>> getParseTreeFromTokens(List<String> tokens, List<String> symbols) {
+        Map<String, List<String>> parseTree = new LinkedHashMap<>();
+        String currentKey = "";
 
-    private final String value;
+        for (String word : tokens) {
+            if (symbols.contains(word.toLowerCase())) {
+                currentKey = word.toUpperCase();
+                parseTree.put(currentKey, new ArrayList<>());
+            } else {
+                parseTree.get(currentKey).add(word);
+            }
+        }
 
-    Lexicon(String value) {
-        this.value = value;
+        return parseTree;
     }
 
-    public String getValue() {
-        return value;
+    private static List<String> getTokenFromString(StringBuilder language) {
+        List<String> tokens = Arrays.asList(language.toString().split("[\\s,\\n]+"));
+        return tokens;
+    }
+
+    private static StringBuilder getLanguageFromSourceCode(Path pathToSourceCode) throws IOException {
+        BufferedReader reader = Files.newBufferedReader(pathToSourceCode);
+        StringBuilder language = new StringBuilder();
+        int ch;
+        while ((ch = reader.read()) != -1) {
+            language.append((char) ch);
+        }
+        reader.close();
+
+        return language;
     }
 }
