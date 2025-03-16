c902e0c8995b7ca2541f4ded8c0491a03fed28ea
Prashant Kumar Singh <prashantsingh090798@gmail.com>
2025-03-16 20:36:04 +0530

    feat: added order by parser

--- Database/Parser/Main.java
+++ Database/Parser/Main.java
@@ -11,10 +11,9 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        String sourceCode = "query2.sql";
+        String sourceCode = "query1.sql";
         Path pathToSourceCode = Paths.get("Database/Query", sourceCode);
-        List<String> symbols = Arrays.asList("select", "from", "where", "order_by", "offset", "limit", "insert_into",
-                "values");
+        List<String> keywords = Symbols.KEYWORDS.getSymbols(); //
 
         try {
             // reading file
@@ -24,7 +23,7 @@ public class Main {
             List<String> tokens = getTokenFromLanguage(language);
 
             // parse tokens based on symbols list
-            Map<String, Object> parseTree = getParseTreeFromTokens(tokens, symbols);
+            Map<String, Object> parseTree = getParseTreeFromTokens(tokens, keywords);
 
             // Print parseTree
             parseTree.forEach((key, value) -> System.out.println(key + " -> " + value));
@@ -34,18 +33,42 @@ public class Main {
         }
     }
 
-    private static Map<String, Object> getParseTreeFromTokens(List<String> tokens, List<String> symbols) {
-        Map<String, Object> parseTree1 = parseForFirstPass(tokens, symbols);
+    private static Map<String, Object> getParseTreeFromTokens(List<String> tokens, List<String> keywords) {
+        Map<String, Object> parseTree1 = parseForFirstPass(tokens, keywords);
 
         Map<String, Object> parseTree2 = new LinkedHashMap<>();
         if (parseTree1.get("WHERE") != null)
             parseTree2 = parseForWherePass(parseTree1);
+        if (parseTree1.get("ORDER_BY") != null)
+            parseForOrderByPass(parseTree1);
         if (parseTree1.get("INSERT_INTO") != null)
             parseTree2 = parseForInsertPass(parseTree1);
 
         // System.out.println("parseTree: " + parseTree2);
 
         return parseTree2;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Map<String, Object> parseForOrderByPass(Map<String, Object> parseTree) {
+        List<String> orderByTokens = (List<String>) parseTree.get("ORDER_BY");
+        List<Map<String, Object>> orderBy = new ArrayList<>();
+        
+        Map<String, Object> currentSubMap = null;
+        for (String orderByToken : orderByTokens) {
+            if (!Symbols.ORDERS.getSymbols().contains(orderByToken.toLowerCase())) {
+                currentSubMap = new LinkedHashMap<>();
+                currentSubMap.put("column", orderByToken);
+                currentSubMap.put("order", "asc");
+                orderBy.add(currentSubMap);
+            } else {
+                currentSubMap.put("order", orderByToken);
+                orderBy.set(orderBy.size() - 1, currentSubMap);
+            }
+        }
+
+        parseTree.put("ORDER_BY", orderBy);
+        return parseTree;
     }
 
     @SuppressWarnings("unchecked")
@@ -79,7 +102,7 @@ public class Main {
     @SuppressWarnings("unchecked")
     private static Map<String, Object> parseForWherePass(Map<String, Object> parseTree) {
         List<String> whereTokens = (List<String>) parseTree.get("WHERE");
-        List<String> operators = Arrays.asList("AND", "OR", "NOT");
+        List<String> operators = Symbols.OPERATORS.getSymbols(); // ->
 
         // Extract where clause
         List<List<String>> where = new ArrayList<>();
@@ -107,12 +130,12 @@ public class Main {
     }
 
     @SuppressWarnings("unchecked")
-    private static Map<String, Object> parseForFirstPass(List<String> tokens, List<String> symbols) {
+    private static Map<String, Object> parseForFirstPass(List<String> tokens, List<String> keywords) {
         Map<String, Object> parseTree = new LinkedHashMap<>();
         String currentKey = "";
 
         for (String word : tokens) {
-            if (symbols.contains(word.toLowerCase())) {
+            if (keywords.contains(word.toLowerCase())) {
                 currentKey = word.toUpperCase();
                 parseTree.put(currentKey, new ArrayList<>());
             } else {
@@ -124,7 +147,10 @@ public class Main {
     }
 
     private static List<String> getTokenFromLanguage(StringBuilder language) {
-        List<String> tokens = Arrays.asList(language.toString().split("[\\s,\\n()]+"));
+        String regex = "[" + Symbols.SEPARATORS.getSymbols().stream()
+                .map(d -> "\\" + d)
+                .collect(Collectors.joining("")) + "]+";
+        List<String> tokens = Arrays.asList(language.toString().split(regex)); // ->
         return tokens;
     }
 
@@ -188,5 +214,23 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+}
+
+enum Symbols {
+    KEYWORDS(Arrays.asList("select", "from", "where", "order_by", "offset", "limit", "insert_into",
+            "values")),
+    OPERATORS(Arrays.asList("AND", "OR", "NOT")),
+    SEPARATORS(Arrays.asList("\n", "\s", ",", "(", ")")),
+    ORDERS(Arrays.asList("asc", "desc"));
+
+    private final List<String> symbols;
+
+    Symbols(List<String> symbols) {
+        this.symbols = symbols;
+    }
+
+    public List<String> getSymbols() {
+        return symbols;
     }
 }
--- Database/Query/query1.sql
+++ Database/Query/query1.sql
@@ -1,6 +1,6 @@
 select id, name, email, password, address 
 from users 
 where id >= 1 AND name == "name1" OR name == "name2" AND NOT address == "address1"
-order_by id
+order_by id desc, name , address desc 
 offset 5
 limit 10
